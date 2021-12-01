4dbcca4df81cf2de8f809ba6127d10c8aa5b3358
Tobias Dersén <tobberoth@gmail.com>
2021-12-01 20:20:47 +0100

    Day 1 completed

---
+++ Day1_SonarSweep/SonarSweepC#/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.WriteLine($"Depth increased {GetBasicIncrease()} times");
+Console.WriteLine($"Window depth increased {GetSlidingWindow()} times");
+
+int GetBasicIncrease()
+{
+  var previousMeasurement = 0;
+  var increaseCount = 0;
+  foreach (var line in File.ReadAllLines("input.txt"))
+  {
+    var measurement = int.Parse(line);
+    if (measurement > previousMeasurement)
+      increaseCount++;
+    previousMeasurement = measurement;
+  }
+  return increaseCount - 1; // Ignore initial increase
+}
+
+int GetSlidingWindow()
+{
+  List<int> window = new();
+  var previousTotal = 0;
+  var increaseCount = 0;
+  foreach (var line in File.ReadAllLines("input.txt"))
+  {
+    var measurement = int.Parse(line);
+    window = window.Append(measurement).ToList();
+    if (window.Count > 3)
+      window.RemoveAt(0);
+    if (window.Count == 3)
+    {
+      var total = window.Sum();
+      if (total > previousTotal)
+        increaseCount++;
+      previousTotal = total;
+    }
+  }
+  return increaseCount - 1; // Ignore initial increase
+}
---
+++ Day1_SonarSweep/SonarSweepF#/Program.fs
@@ -0,0 +1,16 @@
+﻿let diffSum list =
+  list |> List.pairwise |> List.map (fun (x,y) -> if y - x > 0 then 1 else 0) |> List.sum
+
+let diffSumWindow list =
+  let rec loop list acc =
+    match list with
+    | [_;_] -> acc
+    | head :: tail -> (head + tail.Head + tail.Tail.Head) :: loop tail acc
+    | _ -> acc
+  loop list []
+
+let lines = System.IO.File.ReadAllLines("input.txt") |> List.ofArray
+let nums = lines |> List.map (fun x -> x |> int)
+
+printfn "Depth increased %i times" (diffSum nums)
+printfn "Depth window increased %i times" (diffSum (diffSumWindow nums))
