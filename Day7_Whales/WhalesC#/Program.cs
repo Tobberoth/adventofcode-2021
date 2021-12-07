1baa1cb1d45a8eb35a3ff1aa5e5b0a0c14dbe8dc
Tobias Dersén <tobberoth@gmail.com>
2021-12-07 08:28:21 +0100

    Day7 Step 1 Solution done

---
+++ Day7_Whales/WhalesC#/Program.cs
@@ -0,0 +1,21 @@
+﻿var crabPositions = System.IO.File.ReadAllLines("input.txt")[0]
+  .Split(",")
+  .Select(x => int.Parse(x))
+  .ToList();
+
+var resultData = (optimalSteps: Int32.MaxValue, targetPos: 0);
+for (var targetPos = 0; targetPos < crabPositions.Count; targetPos++)
+{
+  var steps = 0;
+  foreach (var crabPos in crabPositions)
+  {
+    steps += Math.Abs(crabPos - targetPos);
+  }
+  if (resultData.optimalSteps > steps)
+  {
+    resultData.optimalSteps = steps;
+    resultData.targetPos = targetPos;
+  }
+}
+
+Console.WriteLine($"Optimal target {resultData.targetPos} with {resultData.optimalSteps} fuel");
