c78c482fac674a018589809a3809e8c89934fa6b
Tobias Dersén <tobberoth@gmail.com>
2021-12-07 08:44:50 +0100

    Day 7 step 2 solved

--- Day7_Whales/WhalesC#/Program.cs
+++ Day7_Whales/WhalesC#/Program.cs
@@ -2,20 +2,41 @@
   .Split(",")
   .Select(x => int.Parse(x))
   .ToList();
+Run(CalculateOne); // pos 345, fuel 348996
+Run(CalculateTwo); // pos 481, fuel 98231647
 
-var resultData = (optimalSteps: Int32.MaxValue, targetPos: 0);
-for (var targetPos = 0; targetPos < crabPositions.Count; targetPos++)
+void Run(Func<int, int, int> calculateFunc)
 {
-  var steps = 0;
-  foreach (var crabPos in crabPositions)
+  var resultData = (optimalSteps: Int32.MaxValue, targetPos: 0);
+  for (var targetPos = 0; targetPos < crabPositions.Count; targetPos++)
   {
-    steps += Math.Abs(crabPos - targetPos);
+    var steps = 0;
+    foreach (var crabPos in crabPositions)
+    {
+      steps += calculateFunc(crabPos, targetPos);
+    }
+    if (resultData.optimalSteps > steps)
+    {
+      resultData.optimalSteps = steps;
+      resultData.targetPos = targetPos;
+    }
   }
-  if (resultData.optimalSteps > steps)
-  {
-    resultData.optimalSteps = steps;
-    resultData.targetPos = targetPos;
-  }
+
+  Console.WriteLine($"Optimal target {resultData.targetPos} with {resultData.optimalSteps} fuel");
 }
 
-Console.WriteLine($"Optimal target {resultData.targetPos} with {resultData.optimalSteps} fuel");
+int CalculateOne(int crabPos, int targetPos)
+{
+  return Math.Abs(crabPos - targetPos);
+}
+
+int CalculateTwo(int crabPos, int targetPos)
+{
+  var diff = Math.Abs(crabPos - targetPos);
+  var result = 0;
+  foreach (var num in Enumerable.Range(1, diff))
+  {
+    result += num;
+  }
+  return result;
+}
