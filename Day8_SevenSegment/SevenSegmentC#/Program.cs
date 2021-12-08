87b9029ed2a1ec0895f50c747fcbaf0361c570ae
Tobias Dersen <Tobias.Dersen@tetrapak.com>
2021-12-08 09:05:02 +0100

    Day 8 Step 1 solution added

---
+++ Day8_SevenSegment/SevenSegmentC#/Program.cs
@@ -0,0 +1,24 @@
+﻿Run1(ReadInput("input.txt"));
+
+List<Analysis> ReadInput(string filename)
+{
+  return System.IO.File.ReadAllLines(filename)
+    .Select(s => new Analysis(s.Split(" | ")[0].Split(' '), s.Split(" | ")[1].Split(' ')))
+    .ToList();
+}
+
+void Run1(List<Analysis> data)
+{
+  var counter = 0;
+  foreach (var analysis in data)
+  {
+    foreach (var outValue in analysis.OutputValues)
+    {
+      if (outValue.Length == 2 || outValue.Length == 3 || outValue.Length == 4 || outValue.Length == 7)
+        counter++;
+    }
+  }
+  Console.WriteLine($"{counter}");
+}
+
+record Analysis(string[] InputValues, string[] OutputValues);
