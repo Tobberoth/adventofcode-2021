2998de8b6b262d1e388b7e22743bad77e636ac06
Tobias Dersen <Tobias.Dersen@tetrapak.com>
2021-12-10 09:53:14 +0100

    Day 10 Step 2 solution done

--- Day10_SyntaxScoring/SyntaxScoringC#/Program.cs
+++ Day10_SyntaxScoring/SyntaxScoringC#/Program.cs
@@ -1,10 +1,33 @@
 ﻿var lines = System.IO.File.ReadAllLines("input.txt");
-var result = 0;
-foreach (var line in lines)
-  result += CheckLine(line);
-Console.WriteLine($"Result: {result}");
+PrintCorruptedScore(lines); // 374061
+PrintIncompleteScore(lines); // 2116639949
 
-int CheckLine(string line)
+void PrintCorruptedScore(string[] lines)
+{
+  var result = 0;
+  foreach (var line in lines)
+  {
+    var points = CheckLineCorrupted(line);
+    result += points;
+  }
+  Console.WriteLine($"Corrupted: {result}");
+}
+
+void PrintIncompleteScore(string[] lines)
+{
+  List<long> result = new();
+  foreach (var line in lines)
+  {
+    var incompleteScore = CheckLineIncomplete(line);
+    if (incompleteScore > 0)
+      result.Add(incompleteScore);
+  }
+  result.Sort();
+  var score = result[(int)Math.Floor(result.Count/2.0)];
+  Console.WriteLine($"Incomplete: {score}");
+}
+
+int CheckLineCorrupted(string line)
 {
   Stack<char> stack = new();
   foreach (var character in line)
@@ -15,59 +38,85 @@ int CheckLine(string line)
     {
       var prev = stack.Pop();
       if (!IsMatch(character, prev))
-      {
-        Console.WriteLine($"CORRUPTED: {line}");
-        return GetPoints(character);
-      }
+        return GetCorruptedScore(character);
     }
   }
   return 0;
 }
 
+long CheckLineIncomplete(string line)
+{
+  Stack<char> stack = new();
+  foreach (var character in line)
+  {
+    if (IsOpen(character))
+      stack.Push(character);
+    else
+    {
+      var prev = stack.Pop();
+      if (!IsMatch(character, prev))
+        return 0;
+    }
+  }
+  long score = 0;
+  while (stack.Count > 0)
+  {
+    var closingChar = stack.Pop();
+    score = score * 5 + GetIncompleteScore(closingChar);
+  }
+  return score;
+}
+
 bool IsOpen(char input)
 {
-  switch (input)
+  return input switch
   {
-    case '(':
-    case '[':
-    case '{':
-    case '<':
-      return true;
-    default:
-      return false;
-  }
+    '(' or '[' or '{' or '<' => true,
+    _ => false
+  };
+}
+
+char GetMatch(char character)
+{
+  return character switch
+  {
+    '(' => ')',
+    ')' => '(',
+    '[' => ']',
+    ']' => '[',
+    '{' => '}',
+    '}' => '{',
+    '<' => '>',
+    '>' => '<',
+    _ => throw new ArgumentException("Invalid character")
+  };
 }
 
 bool IsMatch(char character, char prev)
 {
-  switch (character)
-  {
-    case ')':
-      return prev == '(';
-    case ']':
-      return prev == '[';
-    case '}':
-      return prev == '{';
-    case '>':
-      return prev == '<';
-    default:
-      return false;
-  }
+  return prev == GetMatch(character);
 }
 
-int GetPoints(char character)
+int GetCorruptedScore(char character)
 {
-  switch (character)
+  return character switch
   {
-    case ')':
-      return 3;
-    case ']':
-      return 57;
-    case '}':
-      return 1197;
-    case '>':
-      return 25137;
-    default:
-      return 0;
-  }
+    ')' => 3,
+    ']' => 57,
+    '}' => 1197,
+    '>' => 25137,
+    _ => 0
+  };
+}
+
+int GetIncompleteScore(char character)
+{
+  return character switch
+  {
+    '(' => 1,
+    '[' => 2,
+    '{' => 3,
+    '<' => 4,
+    _ => throw new ArgumentException("Invalid character for scoring")
+  };
 }
